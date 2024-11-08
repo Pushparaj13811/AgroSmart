1d10844a6224b6c8af9a6f902bebba2f1ec278ca
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-11-09 00:33:27 +0530

    feat: add ProtectedRoute for secure access to DiseaseDetection, UserProfile, and AdminDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { About, AdminDashboard, Blog, BlogPost, DiseaseDetection, Features, ForgotPassword, Home, Login, Register, VideoLibrary, } from "./pages"
+import { About, AdminDashboard, Blog, BlogPost, DiseaseDetection, Features, ForgotPassword, Home, Login, Register, UserProfile, VideoLibrary, } from "./pages"
 import { Provider } from 'react-redux';
 import store from './store/store';
+import ProtectedRoute from './services/protectedRoutes';
 
 function App() {
   const location = useLocation();
@@ -22,15 +23,27 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/features" element={<Features />} />
           <Route path="/about" element={<About />} />
-          <Route path="/detect" element={<DiseaseDetection />} />
+          <Route path="/detect" element={
+            <ProtectedRoute>
+              <DiseaseDetection />
+            </ProtectedRoute>
+          } />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:id" element={<BlogPost />} />
           <Route path="/videos" element={<VideoLibrary />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          {/* <Route path="/profile" element={<UserProfile />} /> */}
-          <Route path="/admin/*" element={<AdminDashboard />} />
+          <Route path="/profile" element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/*" element={
+            <ProtectedRoute adminOnly={true}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          } />
 
         </Routes>
       </main>

dd3c08838f7d2df92e83d414a9f638154eea3cff
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-11-09 02:38:35 +0530

    feat: display user information in UserProfile component

--- src/pages/user/UserProfile.tsx
+++ src/pages/user/UserProfile.tsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+
+
 function UserProfile() {
+    const user = useSelector((state: RootState) => state.user.user);
     return (
         <div>
-
+            <h1>User Profile</h1>
+            <p>{user?.email}</p>
+            <p>{user?.fullName}</p>
+            <p>{user?.bio}</p>
         </div>
     )
 }

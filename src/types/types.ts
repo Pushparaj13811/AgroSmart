943448e5a095ae5d81d2ffee154915ebbae7c0aa
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-11-08 14:28:11 +0530

    add types for Feature, BlogPost, and Video to define data structures

---
+++ src/types/types.ts
@@ -0,0 +1,30 @@
+import * as LucideIcons from 'lucide-react';
+
+export type LucideIconName = keyof typeof LucideIcons;
+
+export interface Feature {
+    icon: LucideIconName;
+    title: string;
+    description: string;
+}
+
+export interface BlogPost {
+    id: string;
+    title: string;
+    excerpt: string;
+    image: string;
+    author: string;
+    date: string;
+    category: string;
+
+}
+export interface Video {
+    id: string;
+    thumbnail: string;
+    title: string;
+    videoUrl: string;
+    description: string;
+    duration: string;
+    category: string;
+  }
+  

d242cad560b9b8c45d03e214350c3e68c17f98c7
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-11-09 13:44:31 +0530

    feat: update routing logic to conditionally render UserProfile for logged-in users and enhance CSS with custom properties

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,31 @@ import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { About, AdminDashboard, Blog, BlogPost, DiseaseDetection, Features, ForgotPassword, Home, Login, Register, UserProfile, VideoLibrary, } from "./pages"
-import { Provider } from 'react-redux';
-import store from './store/store';
+import { About, AdminDashboard, Blog, BlogPost, DiseaseDetection, Features, ForgotPassword, Home, Login, Register, UserProfile, VideoLibrary } from "./pages";
+import { Provider, useSelector } from 'react-redux';
+import store, { RootState } from './store/store';
 import ProtectedRoute from './services/protectedRoutes';
-
 function App() {
+  const { user } = useSelector((state: RootState) => state.user);
   const location = useLocation();
+
   useEffect(() => {
-    const pathName = location.pathname.replace("/", "") || "Home"; 
+    const pathName = location.pathname.split('/')[1] || "Home";
     document.title = `AgroSmart - ${pathName.charAt(0).toUpperCase() + pathName.slice(1)}`;
   }, [location]);
 
   return (
-
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* If user is logged in, render UserProfile, else render Home */}
+          <Route path="/" element={user !== null ? <UserProfile /> : <Home />} />
+
           <Route path="/features" element={<Features />} />
           <Route path="/about" element={<About />} />
+
+          {/* Protected routes */}
           <Route path="/detect" element={
             <ProtectedRoute>
               <DiseaseDetection />
@@ -31,20 +35,17 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:id" element={<BlogPost />} />
           <Route path="/videos" element={<VideoLibrary />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={
-            <ProtectedRoute>
-              <UserProfile />
-            </ProtectedRoute>
-          } />
+
+          {/* Protect Admin Dashboard route */}
           <Route path="/admin/*" element={
             <ProtectedRoute adminOnly={true}>
               <AdminDashboard />
             </ProtectedRoute>
           } />
-
         </Routes>
       </main>
       <Footer />

395e4ae937b889ba44681e36621c7ddf9164d128
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-11-11 05:09:38 +0530

    feat: integrate ProfileUpdateForm and enhance routing with AnimatePresence for smoother transitions

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { About, AdminDashboard, Blog, BlogPost, DiseaseDetection, Features, Forg
 import { Provider, useSelector } from 'react-redux';
 import store, { RootState } from './store/store';
 import ProtectedRoute from './services/protectedRoutes';
+import ProfileUpdateForm from './pages/user/ProfileUpdateForm';
+import { AnimatePresence } from 'framer-motion';
+
 function App() {
   const { user } = useSelector((state: RootState) => state.user);
   const location = useLocation();
@@ -19,34 +22,41 @@ function App() {
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       <main>
-        <Routes>
-          {/* If user is logged in, render UserProfile, else render Home */}
-          <Route path="/" element={user !== null ? <UserProfile /> : <Home />} />
+        <AnimatePresence mode="wait" initial={false}>
+          <Routes location={location} key={location.pathname}>
+            {/* If user is logged in, render UserProfile, else render Home */}
+            <Route path="/" element={user !== null ? <UserProfile /> : <Home />} />
 
-          <Route path="/features" element={<Features />} />
-          <Route path="/about" element={<About />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/about" element={<About />} />
 
-          {/* Protected routes */}
-          <Route path="/detect" element={
-            <ProtectedRoute>
-              <DiseaseDetection />
-            </ProtectedRoute>
-          } />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-          <Route path="/videos" element={<VideoLibrary />} />
+            {/* Protected routes */}
+            <Route path="/detect" element={
+              <ProtectedRoute>
+                <DiseaseDetection />
+              </ProtectedRoute>
+            } />
+            <Route path="/update-profile" element={
+              <ProtectedRoute>
+                <ProfileUpdateForm />
+              </ProtectedRoute>
+            } />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+            <Route path="/videos" element={<VideoLibrary />} />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
 
-          {/* Protect Admin Dashboard route */}
-          <Route path="/admin/*" element={
-            <ProtectedRoute adminOnly={true}>
-              <AdminDashboard />
-            </ProtectedRoute>
-          } />
-        </Routes>
+            {/* Protect Admin Dashboard route */}
+            <Route path="/admin/*" element={
+              <ProtectedRoute adminOnly={true}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            } />
+          </Routes>
+        </AnimatePresence>
       </main>
       <Footer />
     </div>

83c559df3fea355997db0916cecc371ce21aeebc
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-11-29 01:19:09 +0530

    feat: add video, AI, and weather slices to the Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
-import cropReducer  from './cropSlice';
+import cropReducer from './cropSlice';
+import videoReducer from './videoSlice'
+import aiSlice from './aiSlice';
+import weatherSlice from './weatherSlice';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         crop: cropReducer,
+        videos: videoReducer,
+        aiSlice: aiSlice,
+        weatherSlice: weatherSlice,
 
     },
 });
